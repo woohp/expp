1858c224cba3b85f76bfa3f80019c166314860f1
Hui Peng Hu <woohp135@gmail.com>
2017-10-15 21:31:50 -0700

    first commit

---
+++ expp.cpp
@@ -0,0 +1,49 @@
+#include <erl_nif.h>
+#include "src/solder.hpp"
+#include "src/stl.hpp"
+using namespace std;
+
+
+vector<int> times(vector<int> v, int i)
+{
+    for (auto& x: v)
+        x *= i;
+    return v;
+}
+
+
+unordered_map<int, int> times2(unordered_map<int, int> m)
+{
+    for (auto& item: m)
+        item.second *= item.first;
+    return m;
+}
+
+
+vector<int> times3(vector<int> m)
+{
+    for (auto& item: m)
+        item *= 3;
+    return m;
+}
+
+
+tuple<int, int, int> times4(tuple<int, int, int> m)
+{
+    return make_tuple(get<0>(m) * 4, get<1>(m) * 4, get<2>(m) * 4);
+}
+
+
+atom catcat(atom a)
+{
+    return atom(a.name + a.name);
+}
+
+
+ELIXIR_MODULE(Foo,
+    def(times, "times"),
+    def(times2, "times2"),
+    def(times3, "times3"),
+    def(times4, "times4"),
+    def(catcat, "catcat")
+);
---
+++ lib/expp.ex
@@ -0,0 +1,30 @@
+defmodule Foo do
+  @moduledoc """
+  Documentation for Foo.
+  """
+
+  @on_load :init
+  def init() do
+    :ok = :erlang.load_nif('expp', 0)
+  end
+
+  def times(_v, _i) do
+    "NIF library not loaded"
+  end
+
+  def times2(_s) do
+    "NIF library not loaded"
+  end
+
+  def times3(_s) do
+    "NIF library not loaded"
+  end
+
+  def times4(_s) do
+    "NIF library not loaded"
+  end
+
+  def catcat(_s) do
+    "NIF library not loaded"
+  end
+end
