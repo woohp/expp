d285b9f46cb1901bb6735cf847f916961089550d
Hui Peng Hu <woohp135@gmail.com>
2023-12-03 17:13:24 -0800

    update elixir to 1.15

--- lib/expp.ex
+++ lib/expp.ex
@@ -5,7 +5,7 @@ defmodule Foo do
 
   @on_load :init
   def init() do
-    :ok = :erlang.load_nif('expp', 0)
+    :ok = :erlang.load_nif(~c"expp", 0)
   end
 
   def vector_times_int(_v, _i), do: "NIF library not loaded"
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Foo.Mixfile do
     [
       app: :expp,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
